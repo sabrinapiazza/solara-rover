3b81019394bc3a8d64fe478bf72bdfcc4fa943e1
Jasmine Ai-Vy Lai <jasminelai@Jasmines-MacBook-Pro.local>
2026-02-09 16:09:24 -0800

    Add thermal camera script for MLX90640

--- env_data/sensors/thermal.py
+++ env_data/sensors/thermal.py
@@ -1,3 +1,37 @@
 # MLX90640 thermal camera driver
 # Reads temperature array (32x24 pixels)
 # Returns thermal image data
+
+import time
+import board
+import busio
+import adafruit_mlx90640
+print ("Thermal sensor libraries not installed.")
+print ("This is expected if not running inside the container!")
+
+def main():
+    i2c = busio.I2C(board.SCL, board.SDA, frequency= 8e+5) #creating an i2c communication channel
+    mlx = adafruit_mlx90640.MLX90640(i2c)
+    mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_8_Hz
+    frame = [0] * 768 #768 are the pixels from the camera
+    print("Thermal camera initallized. Reading data...")
+
+    try:
+        while True:
+            mlx.getFrame(frame)
+
+            min_temp = min(frame)
+            max_temp = max(frame)
+            avg_temp = sum(frame) / len(frame) #calculating average temp using sum of all temperatures and dividing by # of pixels
+            print(
+                f"Min:" {min_temp:.2f} deg C,""
+                f"Max:  {max_temp:.2f} deg C,"
+                f"Avg:  {avg_temp:.2f}  deg C"
+            )
+            time.sleep(1)
+            
+    except KeyboardInterrupt:
+        print("Thermal camera has stopped.")
+
+if __name__ == "__main__":
+    main()
