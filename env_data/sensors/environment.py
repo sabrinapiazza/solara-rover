a8fa58159999ad0915d42adcb7510ce5df5a8db8
Sabrina Piazza <sabrinapiazza@ucr-secure-25-10-13-148-120.wnet.ucr.edu>
2026-03-02 21:26:29 -0800

    light

--- env_data/sensors/environment.py
+++ env_data/sensors/environment.py
@@ -28,7 +28,7 @@ def main():
     #     bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
     # except Exception as e:
     #     print(f"Could not find BME680: {e}")
-    i2c = board.I2C()
+    i2c = busio.I2C(board.SCL, board.SDA)
 
     # To initialise using the default address:
     bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
