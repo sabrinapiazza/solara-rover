7d0ed674ba7c05f180f21561950f9b2022d31c2f
Sanvi Srinivas <31sanvisrinivas@gmail.com>
2026-03-03 20:00:55 -0800

    jd

--- ros2_ws/src/rover_drivers/scripts/test_bno.py
+++ ros2_ws/src/rover_drivers/scripts/test_bno.py
@@ -7,6 +7,8 @@ print(dir(board))  #output should show SCL and SDA
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bno055.BNO055_I2C(i2c, 0x28)
 
-print("Quaternion:", sensor.quaternion)
-print("Gyro:", sensor.gyroscope)
-print("Accel:", sensor.acceleration)
+print([a for a in dir(sensor) if not a.startswith('_')])
+
+# print("Quaternion:", sensor.quaternion)
+# print("Gyro:", sensor.gyroscope)
+# print("Accel:", sensor.acceleration)
