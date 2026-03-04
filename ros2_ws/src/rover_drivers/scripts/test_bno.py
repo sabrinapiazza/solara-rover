52316dbc91c46893e778c47663ff509436e02bd6
Sanvi Srinivas <31sanvisrinivas@gmail.com>
2026-03-03 20:03:21 -0800

    last

--- ros2_ws/src/rover_drivers/scripts/test_bno.py
+++ ros2_ws/src/rover_drivers/scripts/test_bno.py
@@ -1,11 +1,15 @@
 import board
 import adafruit_bno055
 import busio
+import time
 
 print(dir(board))  #output should show SCL and SDA
 
 i2c = busio.I2C(board.SCL, board.SDA)
+time.sleep(0.5)  # give sensor time to settle
+
 sensor = adafruit_bno055.BNO055_I2C(i2c)
+time.sleep(0.5)  # give sensor time to settle
 
 print([a for a in dir(sensor) if not a.startswith('_')])
 
