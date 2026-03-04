d6c722eaec93c8d2631b351e32e492bee0ad30a6
Sanvi Srinivas <31sanvisrinivas@gmail.com>
2026-03-03 19:41:46 -0800

    once more

--- ros2_ws/src/rover_drivers/scripts/test_bno.py
+++ ros2_ws/src/rover_drivers/scripts/test_bno.py
@@ -1,12 +1,10 @@
 import board
 import adafruit_bno055
 import busio
-from adafruit_extended_bus import ExtendedI2C as I2C
 
 print(dir(board))  #output should show SCL and SDA
 
-# i2c = board.I2C()
-i2c = I2C(1)
+i2c = busio.I2C(board.SDA, board.SCL)
 sensor = adafruit_bno055.BNO055(i2c)
 
 print("Quaternion:", sensor.quaternion)
