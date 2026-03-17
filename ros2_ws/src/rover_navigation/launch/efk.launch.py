d5db067e1d788e14e1a22e19a4ac8d2b44914017
Sabrina Piazza <sabrinapiazza@ucr-secure-37-10-13-197-242.wnet.ucr.edu>
2026-03-17 11:57:36 -0700

    wrong path to yaml

--- ros2_ws/src/rover_navigation/launch/efk.launch.py
+++ ros2_ws/src/rover_navigation/launch/efk.launch.py
@@ -4,9 +4,9 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    rover_drivers = get_package_share_directory('rover_drivers')
-    params_file1 = os.path.join(rover_drivers, 'config', 'ekf.yaml')
-    params_file2 = os.path.join(rover_drivers, 'config', 'navsat.yaml')
+    rover_navigation = get_package_share_directory('rover_navigation')
+    params_file1 = os.path.join(rover_navigation, 'config', 'ekf.yaml')
+    params_file2 = os.path.join(rover_navigation, 'config', 'navsat.yaml')
 
 
     return LaunchDescription([
