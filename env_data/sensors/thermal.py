38863086fcbad715c807e1b3fb984115abb03a4d
Sabrina Piazza <sabrinapiazza@ucr-secure-38-10-13-203-49.wnet.ucr.edu>
2026-03-19 18:07:50 -0700

    HZ to hz

--- env_data/sensors/thermal.py
+++ env_data/sensors/thermal.py
@@ -11,7 +11,7 @@ def get_data():
     # Single read of MLX90640 frame. Returns min/max/avg stats as a dict for collector.py.
     i2c = busio.I2C(board.SCL, board.SDA, frequency=4e5)
     mlx = adafruit_mlx90640.MLX90640(i2c)
-    mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_Hz
+    mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
 
     frame = [0] * 768  # 32x24 = 768 pixels
     mlx.getFrame(frame)
