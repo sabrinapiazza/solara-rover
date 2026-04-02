58fb4c45d4e5f05e1881311a6a27dff8dcbe5488
Sanvi Srinivas <31sanvisrinivas@gmail.com>
2026-04-02 16:21:46 -0700

    nndj

--- env_data/old_collector.py
+++ env_data/old_collector.py
@@ -119,14 +119,19 @@ class I2CBus:
         for i in range(len(buf)):
             buf[i] = self._bus.read_byte(addr)
     def writeto_then_readfrom(self, addr, out, buf,
-                               out_start=0, out_end=None,
-                               in_start=0, in_end=None, **kwargs):
+                           out_start=0, out_end=None,
+                           in_start=0, in_end=None, **kwargs):
         out_end  = out_end or len(out)
         in_end   = in_end  or len(buf)
         read_len = in_end - in_start
         reg = out[out_start]
         for i in range(read_len):
-            buf[in_start + i] = self._bus.read_byte_data(addr, reg + i)
+            for attempt in range(5):  # retry up to 5 times
+                try:
+                    buf[in_start + i] = self._bus.read_byte_data(addr, reg + i)
+                    break
+                except OSError:
+                    time.sleep(0.01)  # wait 10ms and retry
 
 i2c = I2CBus(4)  # BNO055 on i2c-4
 sensor = BNO055_I2C(i2c, address=0x28)
