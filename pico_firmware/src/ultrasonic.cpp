6a24f5a466b01eea6c5043db58dd0a0c0bca6ae7
Coralie <cschm028@ucr.edu>
2026-04-08 16:32:55 -0700

    file for motorbridge

--- pico_firmware/src/ultrasonic.cpp
+++ pico_firmware/src/ultrasonic.cpp
@@ -1,4 +1,20 @@
+import rclpy
+from rclpy.node import Node
 
+class MotorBridge(Node):
+    def __init__(self):
+        super().__init__('motor_bridge')
+        self.get_logger().info('Motor bridge node started')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MotorBridge()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
 
 // Reads HC-SR04 sensors (4 sensors)
 // Trigger pulse, measure echo time
