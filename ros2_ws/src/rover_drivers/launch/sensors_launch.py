a4b1059119ce5e0dc20b1abcb92909a91a36cde0
Coralie <cschm028@ucr.edu>
2026-04-18 13:14:31 -0700

    sensor

--- ros2_ws/src/rover_drivers/launch/sensors_launch.py
+++ ros2_ws/src/rover_drivers/launch/sensors_launch.py
@@ -1,8 +1,7 @@
 # Launches all sensor nodes at once
 # Starts GPS driver
 # Starts IMU driver
-
-
+# Starts RPLIDAR
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
@@ -10,7 +9,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     rover_drivers = get_package_share_directory('rover_drivers')
-
 
     return LaunchDescription([
         Node(
@@ -25,6 +23,16 @@ def generate_launch_description():
             name='gps_driver',
             output='screen'
         ),
+        Node(
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            name='rplidar_node',
+            output='screen',
+            parameters=[{
+                'frame_id': 'laser',
+                'angle_compensate': True,
+                'serial_port': '/dev/ttyUSB0',
+                'serial_baudrate': 115200,
+            }]
+        ),
     ])
-
-
