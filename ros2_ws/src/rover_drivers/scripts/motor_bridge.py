25cd47857e1644cff1ceb362f10c4d0e4fe14e49
Coralie <cschm028@ucr.edu>
2026-04-20 17:44:35 -0700

    bridge motor

--- ros2_ws/src/rover_drivers/scripts/motor_bridge.py
+++ ros2_ws/src/rover_drivers/scripts/motor_bridge.py
@@ -69,21 +69,35 @@ class MotorBridge(Node):
         command = f'CMD:{left_pwm},{right_pwm}\n'
         self.serial_port.write(command.encode())
 
+    # def read_serial(self):
+    # if self.serial_port.in_waiting > 0:
+    #     line = self.serial_port.readline().decode().strip()
+    #     # find ENC: anywhere in the line
+    #     if 'ENC:' in line:
+    #         enc_start = line.index('ENC:') + 4
+    #         data = line[enc_start:].split(',')
+    #         if len(data) == 2:
+    #             try:
+    #                 left_ticks = int(data[0])
+    #                 right_ticks = int(data[1])
+    #                 self.update_odometry(left_ticks, right_ticks)
+    #             except ValueError:
+    #                 pass
     def read_serial(self):
-    if self.serial_port.in_waiting > 0:
-        line = self.serial_port.readline().decode().strip()
-        # find ENC: anywhere in the line
-        if 'ENC:' in line:
-            enc_start = line.index('ENC:') + 4
-            data = line[enc_start:].split(',')
-            if len(data) == 2:
-                try:
-                    left_ticks = int(data[0])
-                    right_ticks = int(data[1])
-                    self.update_odometry(left_ticks, right_ticks)
-                except ValueError:
-                    pass
-                
+        if self.serial_port.in_waiting > 0:
+            line = self.serial_port.readline().decode().strip()
+            if 'ENC:' in line:
+                enc_start = line.index('ENC:') + 4
+                data = line[enc_start:].split(',')
+                if len(data) == 2:
+                    try:
+                        left_ticks = int(data[0])
+                        right_ticks = int(data[1])
+                        self.update_odometry(left_ticks, right_ticks)
+                    except ValueError:
+                        pass
+    
+    
     def update_odometry(self, left_ticks, right_ticks):
         # How many ticks since last update
         delta_left_ticks = left_ticks - self.last_left_ticks
