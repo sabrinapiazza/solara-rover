4bf40cb11a377eb263750d53a90231211bfe66d9
Coralie <cschm028@ucr.edu>
2026-04-24 18:21:27 -0700

    motor pinning addition

--- pico_firmware/src/encoder.cpp
+++ pico_firmware/src/encoder.cpp
@@ -4,19 +4,35 @@
 
 #define ENC_A_L  4u
 #define ENC_B_L  5u
+#define ENC_A_R  8u
+#define ENC_B_R  9u
 
 volatile long left_ticks  = 0;
 volatile long right_ticks = 0;
 
 static void encoder_callback(uint gpio, uint32_t events) {
-    bool a = gpio_get(ENC_A_L);
-    bool b = gpio_get(ENC_B_L);
-    if (gpio == ENC_A_L) {
-        if (a == b) left_ticks++;
-        else        left_ticks--;
-    } else {
-        if (a != b) left_ticks++;
-        else        left_ticks--;
+    if (gpio == ENC_A_L || gpio == ENC_B_L) {
+        bool a = gpio_get(ENC_A_L);
+        bool b = gpio_get(ENC_B_L);
+        if (gpio == ENC_A_L) {
+            if (a == b) left_ticks++;
+            else        left_ticks--;
+        } else {
+            if (a != b) left_ticks++;
+            else        left_ticks--;
+        }
+    }
+
+    if (gpio == ENC_A_R || gpio == ENC_B_R) {
+        bool a = gpio_get(ENC_A_R);
+        bool b = gpio_get(ENC_B_R);
+        if (gpio == ENC_A_R) {
+            if (a == b) right_ticks++;
+            else        right_ticks--;
+        } else {
+            if (a != b) right_ticks++;
+            else        right_ticks--;
+        }
     }
 }
 
@@ -29,6 +45,8 @@ static void setup_encoder_pin(uint pin) {
 void encoder_init() {
     setup_encoder_pin(ENC_A_L);
     setup_encoder_pin(ENC_B_L);
+    setup_encoder_pin(ENC_A_R);
+    setup_encoder_pin(ENC_B_R);
 
     gpio_set_irq_enabled_with_callback(
         ENC_A_L,
@@ -37,6 +55,8 @@ void encoder_init() {
         &encoder_callback
     );
     gpio_set_irq_enabled(ENC_B_L, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(ENC_A_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+    gpio_set_irq_enabled(ENC_B_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
 }
 
 long get_left_ticks()  { return left_ticks; }
--- pico_firmware/src/motor_driver.cpp
+++ pico_firmware/src/motor_driver.cpp
@@ -2,8 +2,10 @@
 #include "hardware/pwm.h"
 #include "pico/stdlib.h"
 
-#define RPWM  2u
-#define LPWM  3u
+#define RPWM_L  2u
+#define LPWM_L  3u
+#define RPWM_R  6u
+#define LPWM_R  7u
 #define PWM_WRAP 255u
 
 static void setup_pwm_pin(uint pin) {
@@ -16,26 +18,32 @@ static void setup_pwm_pin(uint pin) {
     pwm_set_gpio_level(pin, 0);
 }
 
-static void set_motor(int pwm_val) {
+static void set_side(uint rpwm, uint lpwm, int pwm_val) {
     if (pwm_val > 0) {
-        pwm_set_gpio_level(RPWM, (uint)pwm_val);
-        pwm_set_gpio_level(LPWM, 0);
+        pwm_set_gpio_level(rpwm, (uint)pwm_val);
+        pwm_set_gpio_level(lpwm, 0);
     } else if (pwm_val < 0) {
-        pwm_set_gpio_level(RPWM, 0);
-        pwm_set_gpio_level(LPWM, (uint)(-pwm_val));
+        pwm_set_gpio_level(rpwm, 0);
+        pwm_set_gpio_level(lpwm, (uint)(-pwm_val));
     } else {
-        pwm_set_gpio_level(RPWM, 0);
-        pwm_set_gpio_level(LPWM, 0);
+        pwm_set_gpio_level(rpwm, 0);
+        pwm_set_gpio_level(lpwm, 0);
     }
 }
 
 void motor_init() {
-    setup_pwm_pin(RPWM);
-    setup_pwm_pin(LPWM);
+    setup_pwm_pin(RPWM_L);
+    setup_pwm_pin(LPWM_L);
+    setup_pwm_pin(RPWM_R);
+    setup_pwm_pin(LPWM_R);
 }
 
 void motor_set(int left_pwm, int right_pwm) {
     if (left_pwm  >  255) left_pwm  =  255;
     if (left_pwm  < -255) left_pwm  = -255;
-    set_motor(left_pwm);
+    if (right_pwm >  255) right_pwm =  255;
+    if (right_pwm < -255) right_pwm = -255;
+
+    set_side(RPWM_L, LPWM_L, left_pwm);
+    set_side(RPWM_R, LPWM_R, right_pwm);
 }
