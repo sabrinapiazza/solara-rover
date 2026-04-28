1faba374efecfff74d777c934fae8dd2d00ae639
Sabrina Piazza <sabrinapiazza@Sabrinas-MacBook-Air-3.local>
2026-04-28 16:02:39 -0700

    changed gps port

--- ros2_ws/src/rover_drivers/scripts/gps_driver.py
+++ ros2_ws/src/rover_drivers/scripts/gps_driver.py
@@ -20,7 +20,8 @@ class GPSDriver(Node):
         super().__init__('gps_driver')
 
         # Parameters
-        self.declare_parameter("port", "/dev/gps")
+        # self.declare_parameter("port", "/dev/gps")
+        self.declare_parameter("port", "/dev/ttyAMA0")
         self.declare_parameter("baud", 9600)
         self.declare_parameter("frame_id", "gps_link")
 
